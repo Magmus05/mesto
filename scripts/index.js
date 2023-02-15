6e43d59b6ed8533448d4d65746ceb50ab440b988
Magomed Musakadiev <musakadiev-mag@yandex.ru>
2023-02-15 22:11:22 +0300

    после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,38 +2,40 @@ let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__container');
+let likeButton = document.querySelector('.element__like-button');
+console.log(likeButton);
 
 function popupOpened(){
 	popup.classList.add('popup_opened');
-}
-
-editButton.addEventListener('click', popupOpened);
-
-let profileName = document.querySelector('.profile__name');
-let profileProfession = document.querySelector('.profile__profession');
-let popupButtonSave = document.querySelector('.popup__button');
-let nameInput = document.querySelector('.popup__name-Input');
-let jobInput = document.querySelector('.popup__job-Input');
-nameInput.value = profileName.textContent;
-jobInput.value = profileProfession.textContent;
-
-function saveValue() {
-	profileName.textContent = nameInput.value;
-	profileProfession.textContent = jobInput.value;
-	popup.classList.remove('popup_opened');
-}
-
-popupButtonSave.addEventListener('click', saveValue);
-
-
-function popupClosed(){
-	popup.classList.remove('popup_opened');
+	let profileName = document.querySelector('.profile__name'); // эти переменные тоже нужно вынести наверх? на мой взгляд они нам не нужны до открытия окна
+	let profileProfession = document.querySelector('.profile__profession');
+	let popupButtonSave = document.querySelector('.popup__button');
+	let nameInput = document.querySelector('.popup__text_type_name');
+	let jobInput = document.querySelector('.popup__text_type_profession');
+	nameInput.value = profileName.textContent;
+	jobInput.value = profileProfession.textContent;
+	
+	function popupClosed(){
+		popup.classList.remove('popup_opened');
 	}
 
+	function saveValue() {
+		profileName.textContent = nameInput.value;
+		profileProfession.textContent = jobInput.value;
+		popupClosed();
+	}
 
-popupClose.addEventListener('click', popupClosed);
+	popupButtonSave.addEventListener('click', saveValue); // возможно событие submit нужно слушать в следующих спринтах, когда эти данные у нас будут в отдельном файле или на сервере, потому как тут при этом событии форма перезагружается и редактированое имя теряется, если я не прав распишите пожалуйста подробней что требуется.
+	popupClose.addEventListener('click', popupClosed);
+}
+function likeButtonActivate(){  // полный функционал, я так понимаю, тоже в следующем спринте, сейчас только возможность закрашивания.
+	let likeButtonActive = document.querySelector('.element__like-button_active');
+	if(likeButtonActive === null){
+		likeButton.classList.add('element__like-button_active');
+	} else{
+			likeButtonActive.classList.remove('element__like-button_active');
+		}
+	}
 
-
-
-
-
+likeButton.addEventListener('click', likeButtonActivate);
+editButton.addEventListener('click', popupOpened);
